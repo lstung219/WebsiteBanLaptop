559f3eef3cea2b485772d2eb798d9263c2414f23
lstung219 <lstung2004@gmail.com>
2024-09-15 22:59:32 +0700

    test
    test

---
+++ WebsiteBanLaptop/Controllers/LaptopController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebsiteBanLaptop.Controllers
+{
+    public class LaptopController : Controller
+    {
+        // GET: Laptop
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult _PartialHeader()
+        {
+            return PartialView();
+        }
+        public ActionResult _PartialFooter() { 
+            return PartialView();
+        }
+    }
+}
