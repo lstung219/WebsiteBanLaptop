06889848532858bd270ddbbafaaa0ea8399d2360
lstung219 <lstung2004@gmail.com>
2024-09-20 11:19:00 +0700

    1 ti admin

---
+++ WebsiteBanLaptop/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebsiteBanLaptop.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin/Admin
+        public ActionResult Admin()
+        {
+            return View();
+        }
+        public ActionResult _PartialFooter()
+        {
+            return PartialView();
+        }
+    }
+}
