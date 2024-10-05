bc02437a7b159d11f63a01ae16c3ee9e0d39a4f0
lstung219 <lstung2004@gmail.com>
2024-10-05 16:43:33 +0700

    tung

--- WebsiteBanLaptop/Controllers/LaptopController.cs
+++ WebsiteBanLaptop/Controllers/LaptopController.cs
@@ -3,19 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebsiteBanLaptop.Models;
 
 namespace WebsiteBanLaptop.Controllers
 {
     public class LaptopController : Controller
     {
         // GET: Laptop
+        db_LAPTOPSTORESEntities db = new db_LAPTOPSTORESEntities();
+        private List<SANPHAM> LayBanNhieu(int count)
+        {
+            return db.SANPHAMs.OrderByDescending(a => a.SLBAN).Take(count).ToList();
+        }
         public ActionResult Index()
         {
-            return View();
+            var listSachBanNhieu = LayBanNhieu(8);
+            return View(listSachBanNhieu);
         }
-        public ActionResult Product()
+        //private List<SANPHAM> LaySachMoi(int count)
+        //{
+        //    return db.SANPHAMs.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        //}
+
+        public ActionResult Product(int? page)
         {
-            return View();
+            int pageSize = 8; // Số sản phẩm trên mỗi trang
+            int pageNumber = page ?? 1; // Nếu không có tham số 'page', thì mặc định là trang 1
+
+            // Lấy tổng số sản phẩm
+            var totalProducts = db.SANPHAMs.Count();
+
+            // Lấy danh sách sản phẩm cho trang hiện tại
+            var sanPhams = db.SANPHAMs
+                            //.Include(sp => sp.THUONGHIEU)
+                            .OrderBy(sp => sp.TENSP)
+                            .Skip((pageNumber - 1) * pageSize) // Bỏ qua các sản phẩm của trang trước
+                            .Take(pageSize) // Lấy số sản phẩm của trang hiện tại
+                            .ToList();
+
+            // Tính tổng số trang
+            ViewBag.TotalPages = (int)Math.Ceiling((double)totalProducts / pageSize);
+            ViewBag.CurrentPage = pageNumber;
+
+            return View(sanPhams);
         }
         public ActionResult About()
         {
@@ -37,6 +67,11 @@ namespace WebsiteBanLaptop.Controllers
         }
         public ActionResult Register() { 
             return View();
+        }
+        public ActionResult ChiTietSP(int id)
+        {
+            var sp = from s in db.SANPHAMs where s.MASP == id select s;
+            return View(sp.Single());
         }
 
     }
